5cf0d0434e015d307029e526d4d8c1a25ffe4985
Peter Pan <peter@never.lan>
2025-06-03 23:26:07 -0400

    remove tooltrim, format icons to 16x16

--- src/main/java/org/evlis/cardinal/Cardinal.java
+++ src/main/java/org/evlis/cardinal/Cardinal.java
@@ -7,8 +7,6 @@ import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.evlis.cardinal.commands.*;
 import org.evlis.cardinal.events.*;
-import org.evlis.cardinal.tooltrims.SmithingTemplate;
-import org.evlis.cardinal.tooltrims.TrimMaterials;
 import org.evlis.cardinal.triggers.Scheduler;
 
 public class Cardinal extends JavaPlugin {
@@ -37,36 +35,11 @@ public class Cardinal extends JavaPlugin {
         playerInteract = new PlayerInteract();
         playerPortal = new PlayerPortal();
         worldChange = new WorldChange();
-        // ToolTrims Setup
-        Set<Material> tools = Set.of(
-                Material.NETHERITE_SWORD,       Material.DIAMOND_SWORD,     Material.GOLDEN_SWORD,      Material.IRON_SWORD,    Material.STONE_SWORD,   Material.WOODEN_SWORD,
-                Material.NETHERITE_PICKAXE,     Material.DIAMOND_PICKAXE,   Material.GOLDEN_PICKAXE,    Material.IRON_PICKAXE,  Material.STONE_PICKAXE, Material.WOODEN_PICKAXE,
-                Material.NETHERITE_AXE,         Material.DIAMOND_AXE,       Material.GOLDEN_AXE,        Material.IRON_AXE,      Material.STONE_AXE,     Material.WOODEN_AXE,
-                Material.NETHERITE_SHOVEL,      Material.DIAMOND_SHOVEL,    Material.GOLDEN_SHOVEL,     Material.IRON_SHOVEL,   Material.STONE_SHOVEL,  Material.WOODEN_SHOVEL,
-                Material.NETHERITE_HOE,         Material.DIAMOND_HOE,       Material.GOLDEN_HOE,        Material.IRON_HOE,      Material.STONE_HOE,     Material.WOODEN_HOE,
-                Material.BOW, Material.CROSSBOW, Material.MACE);
-        int model_data = 311000;
-        for (SmithingTemplate trim: SmithingTemplate.values()) {
-            for (TrimMaterials trim_material: TrimMaterials.values()) {
-
-                model_data++;
-                String trimName = (trim.name() + "_" + trim_material.name()).toLowerCase();
-                TrimEvents.ItemCompatibility.modalDataToTrimName.put(model_data, trimName);
-
-                for (Material tool: tools) {
-                    String key = (tool.name() + "_" + trimName).toLowerCase();
-                    new ToolTrim(
-                            key, tool, trim_material, trim, model_data
-                    );
-                }
-            }
-        }
         // Register Event Listeners
         Bukkit.getServer().getPluginManager().registerEvents(entitySpawn, this);
         Bukkit.getServer().getPluginManager().registerEvents(playerPortal, this);
         Bukkit.getServer().getPluginManager().registerEvents(playerInteract, this);
         Bukkit.getServer().getPluginManager().registerEvents(worldChange, this);
-        Bukkit.getServer().getPluginManager().registerEvents(new TrimEvents(), this);
         // Register Commands
         registerCommands();
     }
